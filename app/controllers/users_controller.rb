8e9382abec1ea4c70a6e75dee0d906e17499b169
khalilhamdii <khaalil.hamdi@gmail.com>
2021-06-06 21:41:00 +0100

    Add friendlist view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def friend_list
+    user = User.find(params[:id])
+    @friendlist = user.friends
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[show]
-  resources :friendships
+  resources :friendships, only: %i[index new create]
+  get 'users/:id/friendlist', to: 'users#friend_list', as: 'friendlist'
+  get 'friendships/destroy', to: 'friendships#destroy'
+  get 'friendships/update', to: 'friendships#update'
   root 'homepage#index'
 end
